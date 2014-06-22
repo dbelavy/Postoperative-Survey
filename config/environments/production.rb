e323f9e20990e78179685359ba379a6d58e2753d
David Belavy <david@belavy.com>
2014-06-22 14:27:23 +1000

    minor errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,14 +89,14 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   
   config.action_mailer.smtp_settings = {
-  address => "smtp.gmail.com",
-  port => 587,
-  domain => "google.com",
-  authentication =>"plain",
-  user_name => ENV(GMAIL_USERNAME),
-  password => EVN(GMAIL_PASSWORD),
-  enable_starttls_auto: true  }
-  }
-
-  
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "google.com",
+    :authentication =>"plain",
+    :user_name => ENV["GMAIL_USERNAME"],
+    :password => ENV["GMAIL_PASSWORD"],
+    :enable_starttls_auto => true  
+    }
 end
+
+
